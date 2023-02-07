e9de799c901f76663f70b83c15bd1d3e9661b681
Wolff Corentin <corentiinwolfft@gmail.com>
2023-02-07 14:14:45 +0100

    Structural start

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-# This is a sample Python script.
+# --------------------------------------------
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Ransomware Project for educational purposes
+# Course : Security integration
+# Bloc : 1
+# Group : IS4
+
+# --------------------------------------------
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print('Todo')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
