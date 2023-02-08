a901760068451a26fda84569937c623489f9c3b8
Wolff Corentin <corentiinwolfft@gmail.com>
2023-02-08 12:03:47 +0100

    Key server done

--- utile/network.py
+++ utile/network.py
@@ -10,46 +10,31 @@
 # Importations
 # --------------------------------------------
 import socket
+from threading import Thread
+from socketserver import ThreadingMixIn
+# --------------------------------------------
+# CLasses
+# --------------------------------------------
+class customThread(Thread):
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print('[+] Nouveau thread démarré pour ' + ip + ":" + str(port))
+
+    def run(self):
+        msg = input('Entrer la réponse du serveur ou STOP pour sortir : ')
+        while msg != "STOP":
+            data = con.recv(2048)
+            print(f'Le serveur a recu des données {data}')
+            msg = input('Entrer la réponse du serveur ou STOP pour sortir : ')
+            if msg == 'STOP':
+                break
+            con.send(bytes(msg, 'utf-8'))
+
 # --------------------------------------------
 # Functions
 # --------------------------------------------
-
-
-def start_key_server(host, port):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while True:
-        server.bind((host, port))
-        server.listen(1)
-
-        client, adressClient = server.accept()
-        print(f'Connexion de {adressClient}')
-
-        data = client.recv(1024)
-        if not data:
-            print(f'Erreur de réception')
-        else:
-            print(f'Reception de : {data}')
-
-            response = data.upper()
-            print(f'Envoi de {response}')
-            n = client.send(response)
-            if (n != len(response)):
-                print("Erreur d'envoi")
-            else:
-                print(f'Envoi ok')
-
-        print(f'Fermeture de la connexion avec le Client.')
-        client.close()
-        print(f'Arret dy serveur.')
-    server.close()
-
-
-def test_server(host, port):
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn.connect((host, port))
-    data = conn.recv(1024)
-    print(f'Received {data}')
-
 
 
 def check_packet_validity(header, message):
@@ -63,3 +48,23 @@ def check_packet_validity(header, message):
     :rtype: bool
     """
     return message[9:9+header]
+
+# --------------------------------------------
+# Program
+# --------------------------------------------
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SOCK_STREAM, 1)
+s.bind(('', 8380))
+threads = []
+
+while True:
+    s.listen(5)
+    print("Serveur: en attente de connexions des clients TCP ...")
+    (con, (ip, port)) = s.accept()
+    mycustomThread = customThread(ip, port)
+    mycustomThread.start()
+    threads.append(mycustomThread)
+
+for t in threads:
+    t.join()
+
--- utile/test_client.py
+++ utile/test_client.py
@@ -1,2 +1,13 @@
-import network
-network.test_server("localhost", 8380)
+import socket
+def test_server(host, port):
+    msg = input(b"ClientA: Entrez un message ou STOP pour sortir:")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    while msg != 'STOP':
+        s.send(bytes(msg,'utf-8'))
+        data = s.recv(2048)
+        print("ClientA a reçu des données:", data)
+        msg = input("Entrez un message pour continuer ou STOP pour sortir:")
+    s.close()
+
+test_server("localhost", 8380)
